ba0a1d5a9422dfbaaa84a2634150c01381919cf0
Deepali <deepalimisale9999@gmail.com>
2021-08-02 10:47:34 +0530

    initial change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -120,19 +120,19 @@ Rails.application.configure do
   #config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
 
-config.action_mailer.delivery_method = :smtp
-host = '#https://trendylist.herokuapp.com' #replace with your own url
-config.action_mailer.default_url_options = {  host: host }
+# config.action_mailer.delivery_method = :smtp
+# host = '#https://trendylist.herokuapp.com' #replace with your own url
+# config.action_mailer.default_url_options = {  host: host }
 
-# SMTP settings for gmail
-config.action_mailer.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-  :user_name            => "deepalimisale2110@gmail.com",
-  :password             => "harikrishna@1",
-  :authentication       => "plain",
-  :enable_starttls_auto => true
-}
+# # SMTP settings for gmail
+# config.action_mailer.smtp_settings = {
+#   :address              => "smtp.gmail.com",
+#   :port                 => 587,
+#   :user_name            => "deepalimisale2110@gmail.com",
+#   :password             => "harikrishna@1",
+#   :authentication       => "plain",
+#   :enable_starttls_auto => true
+# }
 
 end
 #https://trendylist.herokuapp.com
